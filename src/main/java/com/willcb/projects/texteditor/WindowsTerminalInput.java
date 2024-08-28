c24ce012dbbd5703291fbdd031bbb9fcdc046e45
WillCBarker <willbarker34@gmail.com>
2024-08-28 19:56:55 -0400

    Setup for terminal interaction

---
+++ src/main/java/com/willcb/projects/texteditor/WindowsTerminalInput.java
@@ -0,0 +1,12 @@
+package com.willcb.projects.texteditor;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.platform.win32.Kernel32.*;
+import com.sun.jna.platform.win32.Wincon.*;
+import com.sun.jna.platform.win32.WinNT.HANDLE;
+
+public class WindowsTerminalInput {
+
+}
+
