9bab13e7384586a9cb9f2a050626c520e4188ef2
WillCBarker <willbarker34@gmail.com>
2024-09-09 17:33:03 -0400

    Refactored LoadFileIntoBuffer mathod, now interacts through document class

--- src/main/java/com/willcb/projects/texteditor/FileHandler.java
+++ src/main/java/com/willcb/projects/texteditor/FileHandler.java
@@ -3,29 +3,26 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-
 // HANDLES READING FILE INTO GAP BUFFER, SAVING GAP BUFFER INTO FILE, ETC.
 public class FileHandler {
+    
     public static GapBuffer LoadFileIntoBuffer(String filePath) {
         GapBuffer gapBuffer = new GapBuffer(200);
+        Document document = new Document(gapBuffer);
         try {
             File obj = new File(filePath);
             Scanner reader = new Scanner(obj);
-            int lineCount = 0;
             while (reader.hasNextLine()) {
                 String lineData = reader.nextLine();
                 
                 for (int i = 0; i < lineData.length(); i++) {
-                    gapBuffer.insert(lineData.charAt(i), lineCount + i);
+                    document.addCharacter(lineData.charAt(i));
                 }
-                lineCount += lineData.length()+1;
-                gapBuffer.insert('\n', lineCount);
-                lineCount += 1;
+                document.addCharacter('\n');
                 
             }
             reader.close();
             System.out.println("File Loaded:\n");
-            System.out.println(gapBuffer.getGapStart() + " | " + gapBuffer.getGapEnd() + " | " + gapBuffer.getBufferSize());
         } catch (FileNotFoundException e) {
             System.out.println("File Not Found!");
 
