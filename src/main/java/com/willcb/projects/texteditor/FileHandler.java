f6f23cbc091c880da25e6cd6a63d04065974cc6b
WillCBarker <willbarker34@gmail.com>
2024-09-09 15:13:44 -0400

    LoadFile now records newline characters properly

--- src/main/java/com/willcb/projects/texteditor/FileHandler.java
+++ src/main/java/com/willcb/projects/texteditor/FileHandler.java
@@ -11,16 +11,21 @@ public class FileHandler {
         try {
             File obj = new File(filePath);
             Scanner reader = new Scanner(obj);
-            
+            int lineCount = 0;
             while (reader.hasNextLine()) {
                 String lineData = reader.nextLine();
-
+                
                 for (int i = 0; i < lineData.length(); i++) {
-                    gapBuffer.insert(lineData.charAt(i), i);
+                    gapBuffer.insert(lineData.charAt(i), lineCount + i);
                 }
+                lineCount += lineData.length()+1;
+                gapBuffer.insert('\n', lineCount);
+                lineCount += 1;
+                
             }
             reader.close();
-            System.out.println(gapBuffer);
+            System.out.println("File Loaded:\n");
+            System.out.println(gapBuffer.getGapStart() + " | " + gapBuffer.getGapEnd() + " | " + gapBuffer.getBufferSize());
         } catch (FileNotFoundException e) {
             System.out.println("File Not Found!");
 
